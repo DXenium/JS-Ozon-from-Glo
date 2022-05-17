f4a65087b90e5eded932f59a828b649bddc79fac
DudakXen <dudak.xen@yandex.ru>
2022-05-17 20:56:23 +0300

    1 lesson

---
+++ src/modules/cart.js
@@ -0,0 +1,27 @@
+const cart = () => {
+    //объявляем переменные для корзины, модального окна и крестика закрытия модального окна
+
+    const cartBtn = document.getElementById('cart')
+    const cartModal = document.querySelector('.cart')
+    const cartCloseBnt = cartModal.querySelector('.cart-close')
+
+
+    //функция открытия модального окна корзины
+    const openCart = () => {
+        cartModal.style.display = 'flex'
+    }
+
+    //функция закрытия модального окна корзины 
+    const closeCart = () => {
+        cartModal.style.display = ''
+    }
+
+    //отлавливаем события клика 
+    cartBtn.addEventListener('click', openCart)
+    cartCloseBnt.addEventListener('click', closeCart)
+
+}
+
+export default cart
+
+//замкнули переменные в области видимости одной функции
